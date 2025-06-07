36d12c977049a952d722a17585ba759b8f83976b
ronakpanchalk9 <ronakpanchalk9@gmail.com>
2025-06-08 00:53:26 +0530

    update dockerfile and initialise DAG (WIP)

---
+++ dags/reddit_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from datetime import datetime
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+default_args ={
+    'owner': 'Ronak Panchal',
+    'start_date': datetime(2025,6,8)    
+}
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+dag = DAG(
+    dag_id = "etl_reddit_pipeline",
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit','etl','pipeline'],
+)
+
