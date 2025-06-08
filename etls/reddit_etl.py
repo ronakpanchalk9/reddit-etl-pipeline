6c23bce5dfa21a49c323bbf7bb1240c4797392e0
ronakpanchalk9 <ronakpanchalk9@gmail.com>
2025-06-09 00:30:27 +0530

    update utils and reddit_etl

---
+++ etls/reddit_etl.py
@@ -0,0 +1,12 @@
+from praw import Reddit
+
+def connect_reddit(client_id,  client_secret, user_agent) -> Reddit:
+    try:
+        reddit = praw.Reddit(client_id = client_id
+                             client_secret = client_secret,
+                             user_agent = user_agent)
+        print("Connected to Reddit!")
+        return reddit
+    except Exception as e:
+        print(e)
+        sys.exit(1)
